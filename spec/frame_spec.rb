a7d705711da08d6295680b8b5a3a1398788366de
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-22 14:07:00 +0100

    create spec for Frame:MAX_PINS

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,4 +1,11 @@
 require 'frame'
 describe Frame do
 
+  subject(:frame) { described_class.new }
+
+  describe 'Frame::MAX_PINS' do
+    it 'has a maximum of 10 pins' do
+      expect(frame.MAX_PINS).to equal(10)
+    end
+  end
 end
