00ad28835d924c905b4cbbd35fdff257d0c1d692
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-22 11:53:01 +0100

    update #bowl spec to test basic scoring

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,6 +4,8 @@ describe Player do
   subject(:player) {described_class.new}
 
   describe "#bowl" do
-    it { is_expected.to respond_to :bowl }
+    it 'returns a number between 0 and 10' do
+      expect(player.bowl).to be_between(0, 10).inclusive
+    end
   end
 end
