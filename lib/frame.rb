328b8490cb78efe38784c57b16efaf83c25b7414
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-22 21:21:25 +0100

    create #record_frame_score

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,5 +1,14 @@
-class Frame 
+class Frame
 
   MAX_PINS = 10
 
+  attr_reader :history
+
+  def initialize
+    @history = []
+  end
+
+  def record_frame_score(number)
+    history << number
+  end
 end
