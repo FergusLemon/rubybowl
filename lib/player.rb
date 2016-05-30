675e169544083b3e6bb8b37a1e3be7074e7a7b4d
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-30 14:13:29 +0100

    inject frame dependency

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,13 @@
+require_relative 'frame'
 class Player
 
+  attr_reader :frame
+
+  def initialize (frame_klass = Frame.new)
+    @frame = frame_klass
+  end
+
   def bowl
-    rand(11)
+    rand(frame.remaining_pins + 1)
   end
 end
