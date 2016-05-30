e59eb4216744fd0439bd4e3ad403b76471fdad43
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-30 12:07:08 +0100

    udpate spec to include nested array and use "eq" instead of "equal" as not testing for object ID

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -5,7 +5,7 @@ describe Scorecard do
   describe '#record_frame' do
     it 'records a frame' do
       scorecard.record_frame([2, 7])
-      expect(scorecard.history).to equal([2, 7])
+      expect(scorecard.history).to eq([[2, 7]])
     end
   end
 end
