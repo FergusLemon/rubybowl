0c2f24c75418cbc35ee50b83e46a6c4c6228b7e1
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-30 14:01:37 +0100

    create spec for second bowl of a frame

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,18 @@
 require 'player'
 describe Player do
 
-  subject(:player) {described_class.new}
+  subject(:player) { described_class.new }
+  let(:frame) { double :frame }
 
   describe "#bowl" do
     it 'returns a number between 0 and 10' do
-      expect(player.bowl).to be_between(0, 10).inclusive
+      expect(player.bowl).to be_between(0, Frame::MAX_PINS).inclusive
+    end
+
+    context 'second bowl of a frame'
+    it 'returns a number less than or equal to the remaining pins' do
+      allow(frame).to receive(:remaining_pins).and_return(5)
+      expect(player.bowl).to be_between(0, frame.remaining_pins).inclusive
     end
   end
 end
