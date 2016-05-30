b5388705aece1f945f371d51f1aec6e10866ca7e
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-30 14:04:47 +0100

    update #record_frame_score to update #remaining_pins

--- lib/frame.rb
+++ lib/frame.rb
@@ -2,21 +2,25 @@ class Frame
 
   MAX_PINS = 10
 
-  attr_reader :history
+  attr_reader :history, :remaining_pins
 
   def initialize
     @history = []
+    @remaining_pins = MAX_PINS
   end
 
   def record_frame_score(number)
     if history.length == 2
       history.clear
+      remaining_pins == MAX_PINS
       history << number
+      @remaining_pins -= number
     elsif history.length == 0 && number == MAX_PINS
       history << number
       history.clear
     else
       history << number
+      @remaining_pins -= number
     end
   end
 end
