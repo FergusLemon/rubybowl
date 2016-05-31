f094ed9f464bd0047a31be43b884b7d03309b11e
Fergus Lemon <fergus.lemon@gmail.com>
2016-05-31 16:39:50 +0100

    update for #get_score

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,20 @@
 require_relative 'frame'
 class Player
 
-  attr_reader :frame
+  attr_reader :frame, :scorecard
 
-  def initialize (frame_klass = Frame.new)
+  def initialize (frame_klass = Frame.new, scorecard_klass = Scorecard.new)
     @frame = frame_klass
+    @scorecard = scorecard_klass
   end
 
   def bowl
     number = rand(frame.remaining_pins + 1)
     frame.record_frame_score(number)
     number
+  end
+
+  def get_score
+    @scorecard.calculate_current_score
   end
 end
