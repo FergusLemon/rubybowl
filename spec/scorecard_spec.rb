1fcd0b0f07ec8ce243b4a2087c497a0eacdaca06
Fergus Lemon <fergus.lemon@gmail.com>
2016-08-30 18:59:26 +0100

    refactor spec to remove magic numbers 5 & 5 to represent a spare

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,6 +2,7 @@ require 'scorecard'
 describe Scorecard do
   subject(:scorecard) { described_class.new }
   let(:normal_frame) { [2, 7] }
+  let(:spare) { [5, 5] }
 
   describe '#record_frame' do
     it 'records a frame' do
@@ -19,7 +20,7 @@ describe Scorecard do
     end
     context 'includes a spare' do
       it 'adds on the score of the first ball in the next frame' do
-        scorecard.record_frame([5, 5])
+        scorecard.record_frame(spare)
         scorecard.record_frame(normal_frame)
         expect(scorecard.calculate_score).to eq(21)
       end
@@ -35,7 +36,7 @@ describe Scorecard do
 
   describe '#is_spare?' do
     it 'knows what a spare is' do
-      expect(scorecard.is_spare?([5, 5])).to be true
+      expect(scorecard.is_spare?(spare)).to be true
     end
 
     it 'knows a strike is not a spare' do
@@ -52,7 +53,7 @@ describe Scorecard do
   describe '#calculate_bonus' do
     context 'for a spare' do
       it 'adds the following score from the next bowl' do
-        scorecard.record_frame([5, 5])
+        scorecard.record_frame(spare)
         scorecard.record_frame(normal_frame)
         expect(scorecard.calculate_bonus).to eq(2)
       end
