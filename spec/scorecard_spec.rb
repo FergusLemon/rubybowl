dd68b93c7e1f6e86fb729479e26b1f298941671f
Fergus Lemon <fergus.lemon@gmail.com>
2016-08-30 18:57:25 +0100

    refactor spec to remove magic numbers 2 & 7 to represent a normal frame score

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,33 +1,33 @@
 require 'scorecard'
 describe Scorecard do
   subject(:scorecard) { described_class.new }
-  let(:normal_frame) { [2,7] }
+  let(:normal_frame) { [2, 7] }
 
   describe '#record_frame' do
     it 'records a frame' do
       scorecard.record_frame(normal_frame)
-      expect(scorecard.history).to eq([[0, 0], [2, 7]])
+      expect(scorecard.history).to eq([[0, 0], normal_frame])
     end
   end
 
   describe '#calculate_score' do
     context 'no bonus score' do
       it 'calculates the current score' do
-        3.times do scorecard.record_frame([2, 7]) end
+        3.times do scorecard.record_frame(normal_frame) end
         expect(scorecard.calculate_score).to eq(27)
       end
     end
     context 'includes a spare' do
       it 'adds on the score of the first ball in the next frame' do
         scorecard.record_frame([5, 5])
-        scorecard.record_frame([2, 7])
+        scorecard.record_frame(normal_frame)
         expect(scorecard.calculate_score).to eq(21)
       end
     end
     context 'includes a strike' do
       it 'adds on the score of the first and second ball in the next frame' do
         scorecard.record_frame([10, 0])
-        scorecard.record_frame([2, 7])
+        scorecard.record_frame(normal_frame)
         expect(scorecard.calculate_score).to eq(28)
       end
     end
@@ -53,7 +53,7 @@ describe Scorecard do
     context 'for a spare' do
       it 'adds the following score from the next bowl' do
         scorecard.record_frame([5, 5])
-        scorecard.record_frame([2, 7])
+        scorecard.record_frame(normal_frame)
         expect(scorecard.calculate_bonus).to eq(2)
       end
     end
